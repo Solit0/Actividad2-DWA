e93441b8f17e1d3d4a3c598dfc0e740fac97af4a
Luis Balcaceres <luis.balcaceres@catolica.edu.sv>
2025-10-01 22:37:37 -0600

    Agregacion de validaciones

--- src/routes/calificaciones.js
+++ src/routes/calificaciones.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import * as calificacionController from '../controllers/calificacionControllers.js';
+import { createCalificacionValidators, updateCalificacionValidators, runValidations} from '../middlewares/validators.js';
 
 const router = express.Router();
 router.get('/publicacion/:publicacionId', calificacionController.getCalificacionesPorPublicacion);
 router.get('/', calificacionController.getObtenerTodasLasCalificaciones);
 router.get('/:id', calificacionController.getObtenerCalificacionPorId);
-router.post('/', calificacionController.postCrearCalificacion);
-router.put('/:id', calificacionController.putActualizarCalificacion);
+router.post('/', runValidations(createCalificacionValidators), calificacionController.postCrearCalificacion);
+router.put('/:id', runValidations(updateCalificacionValidators), calificacionController.putActualizarCalificacion);
 router.delete('/:id', calificacionController.deleteEliminarCalificacion);
 
 export default router;
